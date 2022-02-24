2315aa19663da94c2df4eec62cbb1f71fc43f592
MKM <mujeebur.rahman@in.ibm.com>
2022-02-24 13:32:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "image" {
 }
 
 variable "profile" {
-  default     = "bc1-2x8"
+  default     = "bx2-2x8"
   description = "The profile of compute CPU and memory resources that you want your VPC virtual servers to have. To list available profiles, run `ibmcloud is instance-profiles`."
 }
 
