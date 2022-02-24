db8ca407989f8840eaa5c4ecf5f61dfa1d13349e
MKM <mujeebur.rahman@in.ibm.com>
2022-02-24 12:35:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,9 @@
 variable "region" {
   description = "The region to create your two VPCs in, such as `us-south`. The VPCs are created in two separate zones within the same region. To get a list of all regions, run `ibmcloud is regions`."
-  default = "us-south"
+  default     = "us-south"
 }
-
 
 provider "ibm" {
   ibmcloud_timeout = 300
-  generation = "1"
+  generation       = "1"
 }
